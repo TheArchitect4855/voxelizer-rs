29d49a3c4d8862d8ec785367929ca0d603fc06f0
Kurtis Knodel <kurtisknodel@gmail.com>
2020-05-26 18:43:56 -0700

    This produces a linker error. I've been fighting with this all day. Send help.

---
+++ src/bindings.rs
@@ -0,0 +1,62 @@
+extern crate cty;
+
+#[repr(C)]
+pub struct vx_vertex {
+	pub v: [f32;3]
+}
+
+#[repr(C)]
+pub struct vx_vec3 {
+	pub x: cty::c_float,
+	pub y: cty::c_float,
+	pub z: cty::c_float
+}
+
+#[repr(C)]
+pub struct vx_color {
+	pub r: cty::c_float,
+	pub g: cty::c_float,
+	pub b: cty::c_float
+}
+
+#[repr(C)]
+pub struct vx_mesh {
+	pub vertices: *mut vx_vertex,
+	pub colors: *mut vx_color,
+	pub normals: *mut vx_vec3,
+	pub indices: *mut cty::c_uint,
+	pub normalindices: *mut cty::c_uint,
+	pub nindices: cty::size_t,
+	pub nnormals: cty::size_t
+}
+
+#[repr(C)]
+pub struct vx_point_cloud {
+	pub vertices: *mut vx_vertex,
+	pub colors: *mut vx_color,
+	pub nvertices: cty::size_t
+}
+
+extern "C" {
+	pub fn vx_voxelize_pc(mesh: *const vx_mesh, 
+		voxelsizex: cty::c_float,
+		voxelsizey: cty::c_float,
+		voxelsizez: cty::c_float,
+		precision: cty::c_float) -> *mut vx_point_cloud;
+
+	pub fn vx_voxelize(mesh: *const vx_mesh, 
+		voxelsizex: cty::c_float,
+		voxelsizey: cty::c_float,
+		voxelsizez: cty::c_float,
+		precision: cty::c_float) -> *mut vx_mesh;
+
+	pub fn vx_voxelize_snap_3dgrid(mesh: *const vx_mesh,
+		width: cty::c_uint,
+		height: cty::c_uint,
+		depth: cty::c_uint) -> *mut cty::c_uint;
+
+	pub fn vx_mesh_alloc(nvertices: cty::c_int, nindices: cty::c_int) -> *mut vx_mesh;
+	pub fn vx_color_mesh_alloc(nvertices: cty::c_int, nindices: cty::c_int) -> *mut vx_mesh;
+	pub fn vx_mesh_free(mesh: *mut vx_mesh);
+	pub fn vx_point_cloud_free(pointcloud: *mut vx_point_cloud);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+mod bindings;
+
+#[test]
+fn test_vx_voxelize_pc() {
+	unsafe {
+		let mesh = bindings::vx_mesh_alloc(16, 16);
+		let _pointcloud = bindings::vx_voxelize_pc(mesh, 1.0, 1.0, 1.0, 1.0);
+		//I'm not totally sure how to check if this stuff is actually valid, so we're just going to do nothing.
+	}
 }
